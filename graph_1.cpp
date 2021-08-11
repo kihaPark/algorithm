00ae3235507065ae95c83abccf8951607d050167
kihaPark <34339280+kihaPark@users.noreply.github.com>
2021-08-12 06:32:23 +0900

    graph, math

---
+++ graph_1.cpp
@@ -0,0 +1,296 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <array>
+
+
+// tested x64, c++17
+
+using namespace std;
+
+// disjoint set algorithm
+// int parent[100]; index = node, value = parent
+
+int GetParent(int* parent, int x)
+{
+    if (x == parent[x])
+        return x;
+
+    return parent[x] = GetParent(parent, parent[x]);
+}
+
+void UnionParent(int* parent, int a, int b)
+{
+    a = GetParent(parent, a);
+    b = GetParent(parent, b);
+    if (a < b)
+        parent[b] = a;
+    else
+        parent[a] = b;
+}
+
+int FindParent(int* parent, int a, int b)
+{
+    a = GetParent(parent, a);
+    b = GetParent(parent, b);
+    return (a == b) ? 1 : 0;
+}
+
+void TestUnionFind()
+{
+    const int NODE_CNT = 11;
+    int parent[NODE_CNT] = { 0, };
+    for (int i = 1; i < NODE_CNT; ++i)
+        parent[i] = i;
+
+    UnionParent(parent, 1, 2);
+    UnionParent(parent, 2, 3);
+    UnionParent(parent, 3, 4);
+    UnionParent(parent, 5, 6);
+    UnionParent(parent, 6, 7);
+    UnionParent(parent, 7, 8);
+
+    for (int i = 1; i < NODE_CNT; ++i)
+        cout << "(" << i << ", " << parent[i] << ") ";
+
+    cout << endl << "1과 5는 연결되어 있나? "
+        << (FindParent(parent, 1, 5) ? "yes" : "no") << endl;
+
+    UnionParent(parent, 1, 5);
+
+    for (int i = 1; i < NODE_CNT; ++i)
+        cout << "(" << i << ", " << parent[i] << ") ";
+
+    cout << endl << "1과 5는 연결되어 있나? "
+        << (FindParent(parent, 1, 5) ? "yes" : "no") << endl;
+}
+
+
+void TestCycle()
+{
+    const int NODE_CNT = 11;
+    int parent[NODE_CNT] = { 0, };
+    for (int i = 1; i < NODE_CNT; ++i)
+        parent[i] = i;
+
+    bool cycle = false;
+    UnionParent(parent, 2, 3);
+
+    for (int i = 1; i < NODE_CNT - 1; ++i) {
+        if (FindParent(parent, i, i + 1)) {
+            cycle = true;
+            break;
+        }
+        else {
+            UnionParent(parent, i, i + 1);
+        }
+    }
+
+    cout << (cycle ? "사이클 발생" : "사이클 없음") << endl;
+
+    for (int i = 1; i < NODE_CNT; ++i)
+        cout << "(" << i << ", " << parent[i] << ") ";
+}
+
+
+// kruskal algorithm, lowest cost spanning tree
+void TestKruskal()
+{
+    const int NODE_CNT = 8;
+    int parent[NODE_CNT] = { 0, };
+    for (int i = 1; i < NODE_CNT; ++i)
+        parent[i] = i;
+
+    // tuple<cost, now_node, next_node>
+    vector<tuple<int, int, int>> edges;
+    edges.push_back(make_tuple(12, 1, 7));
+    edges.push_back(make_tuple(28, 1, 4));
+    edges.push_back(make_tuple(67, 1, 2));
+    edges.push_back(make_tuple(17, 1, 5));
+    edges.push_back(make_tuple(24, 2, 4));
+    edges.push_back(make_tuple(62, 2, 5));
+    edges.push_back(make_tuple(20, 3, 5));
+    edges.push_back(make_tuple(37, 3, 6));
+    edges.push_back(make_tuple(13, 4, 7));
+    edges.push_back(make_tuple(45, 5, 6));
+    edges.push_back(make_tuple(73, 5, 7));
+
+    sort(edges.begin(), edges.end());
+
+    for (auto [cost, now_node, next_node] : edges)
+        cout << cost << ", " << now_node << ", " << next_node << endl;
+
+    int total_cost = 0;
+    for (auto [cost, now_node, next_node] : edges) {
+        if (FindParent(parent, now_node, next_node)) // except cycle
+            continue;
+
+        UnionParent(parent, now_node, next_node);
+        total_cost += cost;
+    }
+
+    cout << "total cost: " << total_cost << endl;
+
+    for (int i = 1; i < NODE_CNT; ++i)
+        cout << "(" << i << ", " << parent[i] << ") ";
+}
+
+
+void TestTopologySort()
+{
+    const int NODE_CNT = 8;
+    vector<int> graph[NODE_CNT];
+    array <int, NODE_CNT> indegree = { 0, };
+
+    graph[1].push_back(2); // node 1 -> node 2
+    indegree[2] += 1;
+    graph[1].push_back(5);
+    indegree[5] += 1;
+    graph[2].push_back(3);
+    indegree[3] += 1;
+    graph[3].push_back(4);
+    indegree[4] += 1;
+    graph[4].push_back(6);
+    indegree[6] += 1;
+    graph[5].push_back(6);
+    indegree[6] += 1;
+    graph[6].push_back(7);
+    indegree[7] += 1;
+
+    queue<int> q;
+    vector<int> result;
+
+    for (int i = 1; i < NODE_CNT; ++i) {
+        if (indegree[i] == 0)
+            q.push(i);
+    }
+
+    while (!q.empty()) {
+        int now_node = q.front();
+        q.pop();
+        result.push_back(now_node);
+
+        for (int next_node : graph[now_node]) {
+            indegree[next_node] -= 1;
+
+            if (indegree[next_node] == 0)
+                q.push(next_node);
+        }
+    }
+
+    cout << endl << "topology sort result: ";
+    for (int node : result)
+        cout << node << ' ';
+    cout << endl;
+}
+
+
+void QuizCityDivisionPlan()
+{
+    // kruskal algorithm, lowest cost spanning tree
+    const int NODE_CNT = 8;
+    int parent[NODE_CNT] = { 0, };
+    for (int i = 1; i < NODE_CNT; ++i)
+        parent[i] = i;
+
+    // tuple<cost, now_node, next_node>
+    vector<tuple<int, int, int>> edges;
+    edges.push_back(make_tuple(12, 1, 7));
+    edges.push_back(make_tuple(28, 1, 4));
+    edges.push_back(make_tuple(67, 1, 2));
+    edges.push_back(make_tuple(17, 1, 5));
+    edges.push_back(make_tuple(24, 2, 4));
+    edges.push_back(make_tuple(62, 2, 5));
+    edges.push_back(make_tuple(20, 3, 5));
+    edges.push_back(make_tuple(37, 3, 6));
+    edges.push_back(make_tuple(13, 4, 7));
+    edges.push_back(make_tuple(45, 5, 6));
+    edges.push_back(make_tuple(73, 5, 7));
+
+    sort(edges.begin(), edges.end());
+
+    for (auto [cost, now_node, next_node] : edges)
+        cout << cost << ", " << now_node << ", " << next_node << endl;
+
+    int total_cost = 0;
+    int last_cost = 0;
+    for (auto [cost, now_node, next_node] : edges) {
+        if (FindParent(parent, now_node, next_node)) // except cycle
+            continue;
+
+        UnionParent(parent, now_node, next_node);
+        total_cost += cost;
+        last_cost = cost;
+    }
+
+    cout << "total cost: " << total_cost
+        << ", last cost: " << last_cost << endl;
+
+    for (int i = 1; i < NODE_CNT; ++i)
+        cout << "(" << i << ", " << parent[i] << ") ";
+}
+
+
+void QuizCurriculumPlan()
+{
+    // topology sort
+    const int NODE_CNT = 8;
+    vector<int> graph[NODE_CNT];
+    array <int, NODE_CNT> indegree = { 0, };
+
+    graph[1].push_back(2); // node 1 -> node 2
+    indegree[2] += 1;
+    graph[1].push_back(5);
+    indegree[5] += 1;
+    graph[2].push_back(3);
+    indegree[3] += 1;
+    graph[3].push_back(4);
+    indegree[4] += 1;
+    graph[4].push_back(6);
+    indegree[6] += 1;
+    graph[5].push_back(6);
+    indegree[6] += 1;
+    graph[6].push_back(7);
+    indegree[7] += 1;
+
+    queue<int> q;
+    vector<int> result;
+
+    for (int i = 1; i < NODE_CNT; ++i) {
+        if (indegree[i] == 0)
+            q.push(i);
+    }
+
+    while (!q.empty()) {
+        int now_node = q.front();
+        q.pop();
+        result.push_back(now_node);
+
+        for (int next_node : graph[now_node]) {
+            indegree[next_node] -= 1;
+
+            if (indegree[next_node] == 0)
+                q.push(next_node);
+        }
+    }
+
+    cout << endl << "quiz curriculum: ";
+
+    for (int node : result)
+        cout << node << ' ';
+}
+
+
+int main()
+{
+    TestUnionFind();
+    TestCycle();
+    TestKruskal();
+    TestTopologySort();
+    QuizCityDivisionPlan();
+    QuizCurriculumPlan();
+
+    return 0;
+}
+
---
+++ math_1.cpp
@@ -0,0 +1,123 @@
+﻿#include <iostream>
+#include <vector>
+#include <array>
+
+
+// tested x64, c++17
+
+using namespace std;
+
+bool IsPrimeNumber(int num)
+{
+    for (int i = 2; i <= (int)sqrt(num); ++i) {
+        if (num % i == 0)
+            return false;
+    }
+    return true;
+}
+
+void TestPrimeNumber()
+{
+    const int MAX_CNT = 5;
+    array<int, MAX_CNT> nums = { 4, 7, 10, 11, 13 };
+    for (int num : nums)
+        cout << num << (IsPrimeNumber(num) ? " : prime num" : " : X") << endl;
+}
+
+
+void TestEratos(int num = 10)
+{
+    // 에라토스테네스의 체
+    // 2 ~ num, find prime number
+    if (num <= 1)
+        return;
+
+    vector<bool> primeNums(num + 1, true); // except 0, 1
+
+    for (int i = 2; i * i <= num; ++i) {
+        if (!primeNums[i])
+            continue;
+
+        for (int j = i * i; j <= num; j += i)
+            primeNums[j] = false;
+    }
+
+    cout << "prime number: ";
+    for (int i = 2; i <= num; i++) {
+        if (primeNums[i])
+            cout << i << ' ';
+    }
+    cout << endl;
+}
+
+
+void TestPartialSum()
+{
+    // two pointers algorithm
+    const int FIND_FIVE = 5;
+    const int MAX_CNT = 5;
+    array<int, MAX_CNT> nums = { 1, 2, 3, 2, 5 };
+
+    int begin = 0, end = 0;
+    int found = 0, partialSum = 0;
+
+    while (true) {
+        if (partialSum == FIND_FIVE)
+            found += 1;
+
+        if (partialSum >= FIND_FIVE)
+            partialSum -= nums[begin++];
+        else if (end == MAX_CNT)
+            break;
+        else
+            partialSum += nums[end++];
+    }
+
+    /*for (; begin < MAX_CNT; ++begin) {
+        while (partialSum < FIND_FIVE && end < MAX_CNT) {
+            partialSum += nums[end];
+            end += 1;
+        }
+
+        if (partialSum == FIND_FIVE)
+            found += 1;
+
+        partialSum -= nums[begin];
+    }*/
+
+    cout << "partial sum count: " << found << endl;
+}
+
+
+void TestPrefixSum()
+{
+    const int MAX_CNT = 5;
+    array<int, MAX_CNT> nums = { 10, 20, 30, 40, 50 };
+    array<int, MAX_CNT> acc = { 0, };
+
+    acc[0] = nums[0];
+    for (int i = 1; i < MAX_CNT; ++i)
+        acc[i] = acc[i - 1] + nums[i];
+
+    for (int num : acc)
+        cout << num << ' ';
+    cout << endl;
+
+    // 0 ~ 2 index prefix sum
+    cout << acc[2] << endl;
+
+    // 2 ~ 3 index prefix sum
+    cout << acc[3] - acc[2 - 1] << endl;
+}
+
+
+int main()
+{
+    TestPrimeNumber();
+    TestEratos();
+    TestPartialSum();
+    TestPrefixSum();
+
+    return 0;
+}
+
